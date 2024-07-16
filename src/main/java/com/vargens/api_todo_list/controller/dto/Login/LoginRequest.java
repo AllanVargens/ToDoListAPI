c216d70796dccac803cd977f8a9525a7c75d1d30
AllanVargens <allanvargens@gmail.com>
2024-07-16 19:19:24 -0300

    feat: route login

---
+++ src/main/java/com/vargens/api_todo_list/controller/dto/Login/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.vargens.api_todo_list.controller.dto.Login;
+
+public record LoginRequest(String username, String password) {
+
+}
---
+++ src/main/java/com/vargens/api_todo_list/controller/dto/Login/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.vargens.api_todo_list.controller.dto.Login;
+
+public record LoginResponse(String accessToken, Long expiresIn) {
+
+}
